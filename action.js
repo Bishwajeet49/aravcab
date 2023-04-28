3fb6104d391e6b9d04f42c41acf6c8d82e70011a
Bishwajeet pandey <bishwajeetpandey49@gmail.com>
2023-04-28 23:59:54 +0530

    Initial commit

---
+++ action.js
@@ -0,0 +1,50 @@
+jQuery(function($) {
+    $(window).on('scroll', function() {
+		if ($(this).scrollTop() >= 200) {
+			$('.navbar').addClass('fixed-top');
+		} else if ($(this).scrollTop() == 0) {
+			$('.navbar').removeClass('fixed-top');
+		}
+	});
+	
+	function adjustNav() {
+		var winWidth = $(window).width(),
+			dropdown = $('.dropdown'),
+			dropdownMenu = $('.dropdown-menu');
+		
+		if (winWidth >= 768) {
+			dropdown.on('mouseenter', function() {
+				$(this).addClass('show')
+					.children(dropdownMenu).addClass('show');
+			});
+			
+			dropdown.on('mouseleave', function() {
+				$(this).removeClass('show')
+					.children(dropdownMenu).removeClass('show');
+			});
+		} else {
+			dropdown.off('mouseenter mouseleave');
+		}
+	}
+	
+	$(window).on('resize', adjustNav);
+	
+	adjustNav();
+});
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+	if ($(this).scrollTop() > 20) {
+	$('#toTopBtn').fadeIn();
+	} else {
+	$('#toTopBtn').fadeOut();
+	}
+	});
+	
+	$('#toTopBtn').click(function() {
+	$("html, body").animate({
+	scrollTop: 0
+	}, 1000);
+	return false;
+	});
+	});
